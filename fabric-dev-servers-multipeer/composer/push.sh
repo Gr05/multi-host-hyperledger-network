ea272dd3c8d14d0cf83841d8b1afb1d715784542
gr05 <lucas.guerry@gmail.com>
2018-04-20 17:31:32 +0200

    Use Env variable

--- fabric-dev-servers-multipeer/composer/push.sh
+++ fabric-dev-servers-multipeer/composer/push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "scp -r crypto-config composer-* ${RASPI_NAME}@${RASPI_IP}:${RASPI_PATH}/multi-host-hyperledger-network/fabric-dev-servers-multipeer/composer"
 
-scp -r crypto-config composer-* pi@10.41.24.170:/home/pi/hyperledger/multi-host-hyperledger-network/fabric-dev-servers-multipeer/composer
+scp -r crypto-config composer-* ${RASPI_NAME}@${RASPI_IP}:${RASPI_PATH}/multi-host-hyperledger-network/fabric-dev-servers-multipeer/composer
--- fabric-dev-servers-multipeer/stopAllFabric.sh
+++ fabric-dev-servers-multipeer/stopAllFabric.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./stopFabric.sh
-ssh pi@10.41.24.170 'cd hyperledger/multi-host-hyperledger-network/fabric-dev-servers-multipeer && ./stopFabric-Peer2.sh'
+ssh ${RASPI_NAME}@${RASPI_IP} "cd ${RASPI_PATH}/multi-host-hyperledger-network/fabric-dev-servers-multipeer && ./stopFabric-Peer2.sh"
