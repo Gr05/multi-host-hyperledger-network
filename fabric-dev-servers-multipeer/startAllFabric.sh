59ea872d4d68e5cc6fa3c5ba2fa30f76a5a9fb46
gr05 <lucas.guerry@gmail.com>
2018-04-20 15:08:28 +0200

    corrections des utilisation de ssh avec PI

--- fabric-dev-servers-multipeer/startAllFabric.sh
+++ fabric-dev-servers-multipeer/startAllFabric.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./startFabric.sh
-ssh pi@10.41.24.170 'cd hyperledger/fabric-samples-1.0.x/fabric-dev-servers-multipeer && ./startFabric-Peer2.sh'
+ssh pi@10.41.24.170 'cd hyperledger/multi-host-hyperledger-network/fabric-dev-servers-multipeer && ./startFabric-Peer2.sh'
--- fabric-dev-servers-multipeer/stopAllFabric.sh
+++ fabric-dev-servers-multipeer/stopAllFabric.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./stopFabric.sh
-ssh pi@10.41.24.170 'cd hyperledger/fabric-samples-1.0.x/fabric-dev-servers-multipeer && ./stopFabric-Peer2.sh'
+ssh pi@10.41.24.170 'cd hyperledger/multi-host-hyperledger-network/fabric-dev-servers-multipeer && ./stopFabric-Peer2.sh'
