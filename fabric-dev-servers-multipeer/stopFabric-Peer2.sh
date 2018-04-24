a622038e6fc51d88280fca6db40b241b16d6afd9
gr05 <lucas.guerry@gmail.com>
2018-04-24 15:14:29 +0200

    Corréction bug restarting network

--- fabric-dev-servers-multipeer/stopFabric-Peer2.sh
+++ fabric-dev-servers-multipeer/stopFabric-Peer2.sh
@@ -11,4 +11,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Shut down the Docker containers that might be currently running.
 cd "${DIR}"/composer
-ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-peer2.yml stop
+ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-peer2.yml down
--- fabric-dev-servers-multipeer/stopFabric.sh
+++ fabric-dev-servers-multipeer/stopFabric.sh
@@ -11,4 +11,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Shut down the Docker containers that might be currently running.
 cd "${DIR}"/composer
-ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose.yml stop
+#ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose.yml stop
+ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose.yml down
