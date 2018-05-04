407a0f0e8a694aaa7a233687bb70db4428f8b573
gr05 <lucas.guerry@gmail.com>
2018-05-04 16:25:44 +0200

    suppresion de l'ancienne version

--- fabric-dev-servers-multipeer/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-composer card delete -n citizen1@sqli-network
-composer card delete -n citizen2@sqli-network
-composer card delete -n sig@sqli-network
-composer card delete -n admin@sqli-network
-
-rm -rf cards/*.card
--- fabric-dev-servers-multipeer/startAllFabric.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./startFabric.sh
-ssh ${RASPI_NAME}@${RASPI_IP} "cd ${RASPI_PATH}/multi-host-hyperledger-network/fabric-dev-servers-multipeer && ./startFabric-Peer2.sh"
